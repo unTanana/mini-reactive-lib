a9d38e81c74ce3ff7c80c0ec11991edabf29f84d
Ciprian Tanana <ciprian.tanana@gmail.com>
2023-07-29 15:51:53 +0300

    working input elements

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: "avoid",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	singleQuote: false,
+	printWidth: 80,
+	semi: true,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: "all",
+};
--- index.js
+++ index.js
@@ -1,48 +1,35 @@
-/*
-    state object = {
-        value: number | string | object | array
-    }
-*/
-
-const states = [];
-
 function updateHtmlElement(stateName, value) {
-    var elements = document.querySelectorAll(`[stateId="${stateName}"]`);
+	var elements = document.querySelectorAll(`[stateId="${stateName}"]`);
 
-    if (elements && elements.length) {
-        elements.forEach((element) => {
-            element.innerHTML = value.toString();
-        });
-    } else {
-        console.warn(`Elements with stateId=${stateName} not found`);
-    }
+	if (elements && elements.length) {
+		elements.forEach(element => {
+			if (element.nodeName !== "INPUT") {
+				element.innerHTML = value.toString();
+			} else {
+				element.value = value.toString();
+			}
+		});
+	} else {
+		console.warn(`Elements with stateId=${stateName} not found`);
+	}
 }
 
 function createState(name, initialValue) {
-    let newState = {
-        name,
-        value: initialValue,
-    };
+	let newState = {
+		name,
+		value: initialValue,
+	};
 
-    // let pushIndex =
-    //     states.push({
-    //         name,
-    //         value: initialValue,
-    //     }) - 1;
-    //
+	updateHtmlElement(name, initialValue);
 
-    updateHtmlElement(name, initialValue);
+	const setter = function (newValue) {
+		newState.value = newValue;
+		updateHtmlElement(name, newValue);
+	};
 
-    const setter = function(newValue) {
-        // states[pushIndex].value = newValue;
-        newState.value = newValue;
-        updateHtmlElement(name, newValue);
-    };
+	const getter = function () {
+		return newState.value;
+	};
 
-    const getter = function() {
-        // return states[pushIndex].value;
-         return newState.value;
-    };
-
-    return [getter, setter];
+	return [getter, setter];
 }
